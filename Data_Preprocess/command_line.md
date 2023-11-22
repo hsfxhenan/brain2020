5f8e91cc705234f9c2110a2d10dd1a00baecaa06
Nan He (Monash University) <nanh@m3-login2.massive.org.au>
2023-11-22 13:10:49 +1100

    edit registration py script suiting the file struc

---
+++ Data_Preprocess/command_line.md
@@ -0,0 +1,5 @@
+# Activate Conda Environment
+
+
+# Dataset Preprocess command line
+python registration.py '/home/nanh/sq58_scratch/nanh/Data/Missing Modalities/ADNI/ADNI/' '/home/nanh/sq58_scratch/nanh/Data/Missing Modalities/ADNI/ADNI_output/'
--- Data_Preprocess/registration.py
+++ Data_Preprocess/registration.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
     if choose_robust_bash_pipeline: # recommend to use robust bash pipeline
         if not os.path.exists(out_folder+'tmp/'): # tmp is used to store intermediate results from the bash pipeline
             os.mkdir(out_folder+'tmp/')
-        for fullPath in glob(raw_folder+'/*.nii'):
+        for fullPath in glob(raw_folder+'*/*/*/*/*.nii'):
             filename = fullPath.split('/')[-1]
             call('bash registration.sh ' + raw_folder + ' ' + filename + ' ' + out_folder, shell=True)
 
     else: # you can also choose to use simple registration function defined above
         # see reference: https://nipype.readthedocs.io/en/0.12.0/interfaces/generated/nipype.interfaces.fsl.preprocess.html#flirt
-        for file in glob(raw_folder+'/*.nii'):
+        for file in glob(raw_folder+'*/*/*/*/*.nii'):
             reference = 'MNI152_T1_1mm.nii.gz'
             registration(file, out_folder, reference)
 
